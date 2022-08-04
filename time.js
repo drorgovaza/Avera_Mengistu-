4ad17ac64ddb116963b002c42783780d0b7aedc2
ingidu dror <govaza1520@gmail.com>
2022-08-05 02:14:58 +0300

    adding scss and typescript

---
+++ time.js
@@ -0,0 +1,25 @@
+"use strict";
+const second = 1000;
+const min = 60 * second;
+const hours = 60 * min;
+const day = 24 * hours;
+const years = 365 * day;
+const CountUp = () => {
+    const countDate = new Date("September 7, 2014 00:00:00").getTime();
+    const ulList = document.querySelector('#ul');
+    const now = new Date().getTime();
+    // How Much time Left since he came here
+    const gap = now - countDate;
+    const current_number_year = Math.floor(gap / years);
+    const current_number_day = Math.floor((gap % years) / day);
+    const current_number_hours = Math.floor((gap % day) / hours);
+    const current_number__minutes = Math.floor((gap % hours) / min);
+    const current_number_seconds = Math.floor((gap % min) / second);
+    ulList.innerHTML = `<li> ${current_number_year}<span> Year </span></li>`;
+    ulList.innerHTML += `<li>${current_number_day}<span> Days </span></li>`;
+    ulList.innerHTML += `<li> ${current_number_hours}<span> Hours </span></li>`;
+    ulList.innerHTML += `<li> ${current_number__minutes}<span> Minutes </span></li>`;
+    ulList.innerHTML += `<li> ${current_number_seconds}<span> Seconds </span></li>`;
+    // divForNumbers.innerHTML=`<h2> S ${current_number_seconds}</h2>`;
+};
+setInterval(CountUp, 1000);
